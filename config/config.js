072a8a27f5df586fd09c63ff97c1212c496bd570
Daryl Carr <darylcarr@live.co.uk>
2017-02-14 15:15:05 +0000

    Add AWS config details

---
+++ config/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  aws: {
+    url: 'https://ffb82joysg.execute-api.us-west-2.amazonaws.com/prod/ncHelpdeskLambda',
+    apiKey: 'iKnE0LsSZM6Zbyj6fm5Q8aFxqkOskwDl849ncg5o'
+  }
+};
